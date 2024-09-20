e1992b297da950f08753a7ebe7ac23ba78f056d3
Kai <khsauian@gmail.com>
2024-09-20 11:22:51 +0800

    feat: special case of sum

--- week02/sum.js
+++ week02/sum.js
@@ -1,7 +1,17 @@
 function sum(ary) {
   // TODO: sum all elements in ary
+  
+  // if ary is a number, return sum of 1 to ary
+  if(typeof(ary) === 'number'){
+    let tmp = []
+    for(let i = 1; i <= ary; i++){
+      tmp.push(i)
+    }
+    return tmp.reduce((acc, cur) => acc + cur, 0)
+  }
   return ary.reduce((acc, cur) => acc + cur, 0)
 }
+
 function sum2(ary) {
   // TODO: sum all elements in ary
   let sum = 0
@@ -25,3 +35,5 @@ console.log(sum([1, 5, 3, 2])) // 11
 console.log(sum2([1, 5, 3, 2])) // 11
 console.log(sum3([1, 5, 3, 2])) // 11
 console.log(sum4([1, 5, 3, 2])) // 11
+// test arr is a number
+console.log(sum(3)) // 6
